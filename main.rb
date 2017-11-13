15f7ff8a8a02a47e4bf7270a970be4d2e2a87302
zhiminzhu <zzm92926@gmail.com>
2017-11-12 21:11:38 -0800

    env

--- main.rb
+++ main.rb
@@ -18,6 +18,15 @@ configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
 
+configure :development, :text do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, 
+    ENV['DATABASE_URL'])
+end
+
 configure do
   enable :session
   set :username, "yuan"
